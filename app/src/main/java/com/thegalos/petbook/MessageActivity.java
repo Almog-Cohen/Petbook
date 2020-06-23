dad6f6519d5cdce6a787e8ede07f4934224237a4
Almog Cohen <almogco94@gmail.com>
2020-06-23 18:16:07 +0300

    Merge branch 'master' of C:\Users\Almog Cohen\StudioProjects\PetBook with conflicts.

--- app/src/main/java/com/thegalos/petbook/MessageActivity.java
+++ app/src/main/java/com/thegalos/petbook/MessageActivity.java
@@ -1,13 +1,21 @@
 package com.thegalos.petbook;
 
+<<<<<<< HEAD
 import android.os.Bundle;
 import android.util.Log;
+=======
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+>>>>>>> Merge branch 'master' of C:\Users\Almog Cohen\StudioProjects\PetBook with conflicts.
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+<<<<<<< HEAD
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -26,10 +34,19 @@ import com.thegalos.petbook.objects.Chat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+=======
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+>>>>>>> Merge branch 'master' of C:\Users\Almog Cohen\StudioProjects\PetBook with conflicts.
 
 public class MessageActivity extends AppCompatActivity {
 
 
+<<<<<<< HEAD
 
     DatabaseReference reference;
     TextView userNameTv;
@@ -37,6 +54,11 @@ public class MessageActivity extends AppCompatActivity {
     MessageAdapter messageAdapter;
     List<Chat> chatList;
     RecyclerView recyclerView;
+=======
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
+    TextView userName;
+>>>>>>> Merge branch 'master' of C:\Users\Almog Cohen\StudioProjects\PetBook with conflicts.
 
     ImageButton sendBtn;
     EditText messageEt;
@@ -48,6 +70,7 @@ public class MessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
+<<<<<<< HEAD
         userNameTv=findViewById(R.id.user_name_tv);
         messageEt=findViewById(R.id.msg_et);
         sendBtn=findViewById(R.id.btn_send);
@@ -57,6 +80,11 @@ public class MessageActivity extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
         linearLayoutManager.setStackFromEnd(true);
         recyclerView.setLayoutManager(linearLayoutManager);
+=======
+        userName=findViewById(R.id.user_name_tv);
+        messageEt=findViewById(R.id.msg_et);
+        sendBtn=findViewById(R.id.btn_send);
+>>>>>>> Merge branch 'master' of C:\Users\Almog Cohen\StudioProjects\PetBook with conflicts.
         ownerId = getIntent().getStringExtra("ownerId");
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -67,6 +95,7 @@ public class MessageActivity extends AppCompatActivity {
 
 
 
+<<<<<<< HEAD
 
         reference = FirebaseDatabase.getInstance().getReference("Users").child(ownerId).child("Details").child("Name");
         reference.addValueEventListener(new ValueEventListener() {
@@ -85,6 +114,9 @@ public class MessageActivity extends AppCompatActivity {
 
             }
         });
+=======
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+>>>>>>> Merge branch 'master' of C:\Users\Almog Cohen\StudioProjects\PetBook with conflicts.
         /*reference = FirebaseDatabase.getInstance().getReference("Users").child(userId);*/
 
         sendBtn.setOnClickListener(new View.OnClickListener() {
@@ -99,7 +131,10 @@ public class MessageActivity extends AppCompatActivity {
                 }
 
                 messageEt.setText("");
+<<<<<<< HEAD
 
+=======
+>>>>>>> Merge branch 'master' of C:\Users\Almog Cohen\StudioProjects\PetBook with conflicts.
             }
         });
 
@@ -108,13 +143,17 @@ public class MessageActivity extends AppCompatActivity {
 
 
 
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> Merge branch 'master' of C:\Users\Almog Cohen\StudioProjects\PetBook with conflicts.
     }
 
     private void sendMessage(String sender,String receiver, String message){
 
         HashMap<String,Object> hashMap = new HashMap<>();
+<<<<<<< HEAD
         hashMap.put("sender", sender);
         hashMap.put("receiver",receiver);
         hashMap.put("message",message);
@@ -155,6 +194,13 @@ public class MessageActivity extends AppCompatActivity {
 
             }
         });
+=======
+        hashMap.put("Sender", sender);
+        hashMap.put("receiver",receiver);
+        hashMap.put("message",message);
+
+        DatabaseReference chatRef = FirebaseDatabase.getInstance().getReference().child(ownerId).child(userId);
+>>>>>>> Merge branch 'master' of C:\Users\Almog Cohen\StudioProjects\PetBook with conflicts.
 
 
     }
